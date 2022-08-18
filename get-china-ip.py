28209b7976f0116e9b6a5222431965418302ae47
PikuZheng <cba321123@gmail.com>
2022-08-18 08:51:13 +0800

    try to delete old lists first

--- get-china-ip.py
+++ get-china-ip.py
@@ -15,7 +15,7 @@ with closing(requests.get('http://ftp.apnic.net/stats/apnic/delegated-apnic-late
   total=0
   for gen_lines in response:
     total += len(gen_lines)
-    print("\rread ip list from apnic...%0.1f%%" % (float(total/content_size) * 100) , end='')
+#    print("\rread ip list from apnic...%0.1f%%" % (float(total/content_size) * 100) , end='')
     try:
       l=str(gen_lines).split("|")
       if (l[1]=="CN" and l[2][0:2]=="ip"):
@@ -28,13 +28,15 @@ print ("\b\b\b\b\b\b...done")
 
 print ("translate ip to rsc...", end="")
 with codecs.open('china-ip-ranges-ipv4-'+str_update+'.txt', 'w' ,"utf-8") as output_ipv4, codecs.open('china-ip-ranges-ipv6-'+str_update+'.txt', 'w' ,"utf-8") as output_ipv6:
+  output_ipv4.write('/ip fir add remove [/ip fir add find comment~"^China20[0-9]*"]'+"\r\n")
+  output_ipv6.write('/ipv6 fir add remove [/ipv6 fir add find comment~"^China20[0-9]*"]'+"\r\n")
   for ip_range in ipset:
     try:
       if ip_range.version() == 4:
 #        print(ip_range.strNormal())
-        output_ipv4.write('/ip fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+"\r\n")
+        output_ipv4.write(':do { /ip fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+" } on-error={}\r\n")
       else:
-        output_ipv6.write('/ipv6 fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+"\r\n")
+        output_ipv6.write(':do { /ipv6 fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+" } on-error={}\r\n")
     except:
       pass
 print ("done")
