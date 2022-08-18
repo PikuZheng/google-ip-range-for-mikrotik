4dca264e6c90bc4816fd1fe804a4a94cdf86d5a7
PikuZheng <cba321123@gmail.com>
2022-08-18 09:02:09 +0800

    try delete old lists first

--- get-google-ip-simple.py
+++ get-google-ip-simple.py
@@ -10,7 +10,9 @@ data.append(json.loads(r.text))
 r = requests.get('https://www.gstatic.com/ipranges/cloud.json')
 data.append(json.loads(r.text))
 
-file_object = codecs.open('google-ip-ranges-simple-'+data[0]['creationTime'][0:10]+'.txt', 'w' ,"utf-8")
+file_object = codecs.open('google-ip-ranges.txt', 'w' ,"utf-8")
+file_object.write('/ip fir add remove [/ip fir add find comment~"^GOOGLE LLC .*"]'+"\r\n")
+file_object.write('/ipv6 fir add remove [/ip fir add find comment~"^GOOGLE LLC .*"]'+"\r\n")
 
 ipset = IPSet()
 for dat in data:
@@ -26,9 +28,9 @@ for dat in data:
 for ip_ranges in ipset:
   try:
     if ip_ranges.version() == 4:
-      file_object.write('/ip fir add add add='+ip_ranges.strNormal()+' comment="GOOGLE LLC '+data[0]['creationTime'][0:10]+'" list=dst-use-vpn'+"\r\n")
+      file_object.write(':do { /ip fir add add add='+ip_ranges.strNormal()+' comment="GOOGLE LLC '+data[0]['creationTime'][0:10]+'" list=dst-use-vpn'+" } on-error={}\r\n")
     else:
-      file_object.write('/ipv6 fir add add add='+ip_ranges.strNormal()+' comment="GOOGLE LLC '+data[0]['creationTime'][0:10]+'" list=dst-use-vpn'+"\r\n")
+      file_object.write(':do { /ipv6 fir add add add='+ip_ranges.strNormal()+' comment="GOOGLE LLC '+data[0]['creationTime'][0:10]+'" list=dst-use-vpn'+" } on-error={}\r\n")
   except:
     pass
 
