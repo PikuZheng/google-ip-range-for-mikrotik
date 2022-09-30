ab26027767eee02cd3053cc207f295b884e80b97
PikuZheng <cba321123@gmail.com>
2022-09-30 10:26:07 +0800

    Update get-china-ip.py

--- get-china-ip.py
+++ get-china-ip.py
@@ -1,43 +1,31 @@
 import requests
 import codecs
-from IPy import IPSet, IP
 from contextlib import closing
-import math
+import datetime
 
 ipset = IPSet()
-str_update = ""
+str_update = datetime.datetime.now().strftime('%Y-%m-%d')
 
-print ("read ip list from apnic...", end="")
-with closing(requests.get('https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/china.txt', stream=True)) as r:
+print ("read ip list from github...", end="")
+with closing(requests.get('https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/china.txt', stream=True)) as r, codecs.open('china-ip-ranges-ipv4.txt', 'w' ,"utf-8") as output_ipv4:
   r.encoding='utf-8'
-  content_size = int(r.headers['content-length'])
   response = r.iter_lines()
-  total=0
-  for gen_lines in response:
-    total += len(gen_lines)
-#    print("\rread ip list from apnic...%0.1f%%" % (float(total/content_size) * 100) , end='')
+  output_ipv4.write('/ip fir add remove [/ip fir add find comment~"^China20[0-9]*"]'+"\r\n")
+  for ip_range in response:
     try:
-#      l=str(gen_lines).split("|")
-#      if (l[1]=="CN" and l[2][0:2]=="ip"):
-#        ipset.add(IP( l[3]+"/"+str(int(32-math.log(int(l[4]))/math.log(2))), make_net = True ))
-#      elif (l[1]=="apnic"):
-#        str_update=l[2]
-      ipset.add(IP(str(gen_lines), make_net = True ))
+#        print(ip_range.strNormal())
+        output_ipv4.write(':do { /ip fir add add add='+ip_range.decode()+' comment="China'+str_update+'" list=dst-use-no-vpn'+" } on-error={}\r\n")
     except:
       pass
-print ("\b\b\b\b\b\b...done")
+print ("done")
 
-print ("translate ip to rsc...", end="")
-with codecs.open('china-ip-ranges-ipv4.txt', 'w' ,"utf-8") as output_ipv4, codecs.open('china-ip-ranges-ipv6.txt', 'w' ,"utf-8") as output_ipv6:
-  output_ipv4.write('/ip fir add remove [/ip fir add find comment~"^China20[0-9]*"]'+"\r\n")
+with closing(requests.get('https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/china6.txt', stream=True)) as r, codecs.open('china-ip-ranges-ipv6.txt', 'w' ,"utf-8") as output_ipv6:
+  r.encoding='utf-8'
+  response = r.iter_lines()
   output_ipv6.write('/ipv6 fir add remove [/ipv6 fir add find comment~"^China20[0-9]*"]'+"\r\n")
-  for ip_range in ipset:
+  for ip_range in response:
     try:
-      if ip_range.version() == 4:
-#        print(ip_range.strNormal())
-        output_ipv4.write(':do { /ip fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+" } on-error={}\r\n")
-      else:
-        output_ipv6.write(':do { /ipv6 fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+" } on-error={}\r\n")
+        output_ipv6.write('/ipv6 fir add add add='+ip_range.strNormal()+' comment="China'+str_update+'" list=dst-use-no-vpn'+"\r\n")
     except:
       pass
 print ("done")
